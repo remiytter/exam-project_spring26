d9757f4d3e1a0a94303e67de2400a555fce8dcaf
remiytter <remiytter@gmail.com>
2026-05-23 14:16:41 +0200

    Update header.js

--- js/header.js
+++ js/header.js
@@ -7,7 +7,7 @@ function logout() {
     localStorage.removeItem("token");
     localStorage.removeItem("user");
 
-    window.location.href = "/index.html";
+    window.location.href = "../index.html";
 }
 
 function renderHeaderActions() {
